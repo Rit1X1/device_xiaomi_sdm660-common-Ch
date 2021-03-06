ae36b2c6fe00a70a80faafda03708b5cbddc0473
CHRISL7 <70589286+ChrisW444@users.noreply.github.com>
2021-03-06 12:23:42 -0400

    sdm660-common: fix local packages/overlays

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,10 +4,10 @@ export SKIP_ABI_CHECKS=true
 # rm -rf
 rm -rf vendor/codeaurora/telephony
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlay
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayWideBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayNarrowBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayExtraWideBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlay
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayWideBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayNarrowBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayExtraWideBack
 
 # clone
 git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
