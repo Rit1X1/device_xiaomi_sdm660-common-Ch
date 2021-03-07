68aeb4f79dadf043386bf8646ffd691b1ca9a545
Rit1X1 <prdrit777@gmail.com>
2021-03-07 22:14:12 +0530

    modification

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,12 +4,8 @@ export SKIP_ABI_CHECKS=true
 # rm -rf
 rm -rf vendor/codeaurora/telephony
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
-rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlay
-rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayWideBack
-rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayNarrowBack
-rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayExtraWideBack
 
 # clone
 git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
-git clone https://github.com/ChrisW444/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
+git clone https://github.com/Rit1X1/android_vendor_codeaurora_telephony.git vendor/codeaurora/telephony
 git clone https://github.com/ChrisW444/vendor_xiaomi_dirac vendor/xiaomi/dirac
