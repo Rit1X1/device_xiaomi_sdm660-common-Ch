18b69b99b8919b15674d9286db5d9b7a4d060695
Rit1X1 <prdrit777@gmail.com>
2021-03-11 17:39:44 +0530

    Revert "fixing gesture nav"
    This reverts commit 89343d6eb7f691861cf1d47e4986dee4c1fe236f.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,8 +9,3 @@ rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
 git clone https://github.com/Rit1X1/android_vendor_codeaurora_telephony.git vendor/codeaurora/telephony
 git clone https://github.com/ChrisW444/vendor_xiaomi_dirac vendor/xiaomi/dirac
-
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlay
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayWideBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayNarrowBack
-rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayExtraWideBack
